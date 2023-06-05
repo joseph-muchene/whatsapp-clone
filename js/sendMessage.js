e2931e9af4a268e397b0f8b2fbb769062a902533
EasyCodeCoach <ngugimuchene002@gmail>
2023-06-05 14:12:51 +0300

    removed orderby on messages

--- js/sendMessage.js
+++ js/sendMessage.js
@@ -135,7 +135,7 @@ window.handleUser = function (id) {
       firebase
         .firestore()
         .collection("messages")
-        .orderBy("timeStamp", "desc")
+
         .onSnapshot((messageSnapshot) => {
           let content = "";
           messageSnapshot.forEach((message) => {
