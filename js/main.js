623795743590aa98b5ec6df525b651879df4fded
EasyCodeCoach <ngugimuchene002@gmail>
2023-06-19 10:49:40 +0300

    refractor code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 // get all users
 
-// firebase
-//   .firestore()
-//   .collection("users")
-//   .get((querySnapshot) => {
-//     const user = querySnapshot.data();
-//   });
-
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
